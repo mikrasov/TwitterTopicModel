6d2f052287938dc68a518b59a7ec81014187efaf
Michael Nekrasov <mikrasov@cs.ucsb.edu>
2015-03-05 10:05:41 -0800

    Add D3 vizualization
    Add D3 framework and proto egonet

---
+++ javascripts/data-egonet.js
@@ -0,0 +1,16 @@
+var data_egonet = {
+	"nodes":[
+                    {"proportions": [{"group": 1, "value": 1},
+                                     {"group": 2, "value": 2},
+                                     {"group": 3, "value": 3}]},
+					{"proportions": [{"group": 1, "value": 7},
+                                     {"group": 2, "value": 1},
+                                     {"group": 3, "value": 2}]},
+                    {"proportions": [{"group": 1, "value": 2},
+                                     {"group": 2, "value": 2},
+                                     {"group": 3, "value": 2}]}],
+              "links":[
+				{"source": 0, "target": 1, "length": 500, "width": 1},
+				{"source": 0, "target": 2, "length": 500, "width": 1}
+			 ]
+ };
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,55 @@
-console.log('This would be the main JS file.');
+
+
+    var width = 960,
+    height = 500,
+    radius = 25,
+    color = d3.scale.category10();
+
+    var pie = d3.layout.pie()
+        .sort(null)
+        .value(function(d) { return d.value; });
+
+    var arc = d3.svg.arc()
+        .outerRadius(radius)
+        .innerRadius(0);
+
+    var svg = d3.select("#graph-egonet").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    var force = d3.layout.force()
+        .charge(-120)
+        .linkDistance(4 * radius)
+        .size([width, height]);
+
+    force.nodes(data_egonet.nodes)
+         .links(data_egonet.links)
+         .start();
+
+    var link = svg.selectAll(".link")
+        .data(data_egonet.links)
+        .enter().append("line")
+        .attr("class", "link");
+
+    var node = svg.selectAll(".node")
+        .data(data_egonet.nodes)
+        .enter().append("g")
+        .attr("class", "node");
+
+    node.selectAll("path")
+        .data(function(d, i) {return pie(d.proportions); })
+        .enter()
+        .append("svg:path")
+        .attr("d", arc)
+        .attr("fill", function(d, i) { return color(d.data.group); });;
+
+    force.on("tick", function() {
+        link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+        node.attr("x", function(d) { return d.x; })
+            .attr("y", function(d) { return d.y; })
+            .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"});
+    });
